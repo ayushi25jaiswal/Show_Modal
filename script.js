d929c82bef279d140d9afda8bf6ef6af9eb672ed
ayushi jaiswal <ayushijaiswal2507@gmail.com>
2023-09-17 23:40:55 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+console.log(btnShowModal);
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModel = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel);
